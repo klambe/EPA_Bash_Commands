7c45e20a971be2a0b051fad4fca20aabd24c5b3f
klambe17@gmail.com <klambe17@gmail.com>
2017-09-29 10:55:34 +0100

    saving progress for lab 2

--- week2/week_2.sh
+++ week2/week_2.sh
@@ -3,11 +3,25 @@ echo "What is your name?"
 read username
 echo "Where are you?"
 read userCity
-echo "Name of your dog?"
-read userDog
+echo "What College are you in?"
+read userCollege
 
-echo "Your name is: $username, you are in $userCity and your dog is $userDog"
+echo "Your name is: $username, you are in $userCity and you are enrolled in ITT."
+echo"Is this information correct?"
+read userResponse
 
+if [userResponse != "yes"];then
+	exit
+fi
 
+# cat /proc/cpuinfo |grep processor | wc -l
+cat /proc/cpuinfo |grep processor | wc -l
 
+num_cpus=$(cat /proc/cpuinfo |grep processor | wc -l)
+
+if [ $num_cpus -eq 1 ]; then
+	echo "You have $num_cpus cpu's in your system."
+else
+	echo "You have more than 1 cpu in your machine!"
+fi
 
